5fa9241721ecd5919b75bc6905a28d87e1b9878e
Vasishth Gupta <vasishthemail@gmail.com>
2022-10-01 02:54:22 +0530

    development-push-008
    works nai hore

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,7 +7,6 @@ $(function () {
 
 
 // cursor
-const $bigBall = document.querySelector('.cursor__ball--big');
 const $smallBall = document.querySelector('.cursor__ball--small');
 const $hoverables = document.querySelectorAll('.hoverable');
 
@@ -21,11 +20,7 @@ for (let i = 0; i < $hoverables.length; i++) {
 
 // Move the cursor
 function onMouseMove(e) {
-  TweenMax.to($bigBall, .4, {
-    x: e.clientX - 15,
-    y: e.clientY - 15
-    
-  })
+  
   TweenMax.to($smallBall, .1, {
     x: e.clientX - 5,
     y: e.clientY - 5
@@ -34,12 +29,12 @@ function onMouseMove(e) {
 
 // Hover an element
 function onMouseHover() {
-  TweenMax.to($bigBall, .3, {
-    scale: 3
+  TweenMax.to($smallBall, .5, {
+    scale: 5
   })
 }
 function onMouseHoverOut() {
-  TweenMax.to($bigBall, .3, {
+  TweenMax.to($smallBall, .3, {
     scale: 1
   })
 }
