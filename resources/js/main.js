8a2e4b70b174d4286fd856a963b977505def74f5
Vasishth Gupta <vasishthemail@gmail.com>
2022-10-01 09:25:57 +0530

    development-push-013

--- resources/js/main.js
+++ resources/js/main.js
@@ -106,3 +106,35 @@ $(".to-mail").click(function(){
 $(".to-github").click(function(){
   window.open("https://github.com/vasishthg")
 })
+
+(function(){
+  function id(v){ return document.getElementById(v); }
+  function loadbar() {
+    var ovrl = id("overlay"),
+        stat = id("progstat"),
+        img = document.images,
+        c = 0,
+        tot = img.length;
+    if(tot == 0) return doneLoading();
+
+    function imgLoaded(){
+      c += 1;
+      var perc = ((100/tot*c) << 0) ;
+      stat.innerHTML = perc;
+      if(c===tot) return doneLoading();
+    }
+    function doneLoading(){
+      ovrl.style.opacity = 0;
+      setTimeout(function(){ 
+        ovrl.style.display = "none";
+      }, 1200);
+    }
+    for(var i=0; i<tot; i++) {
+      var tImg     = new Image();
+      tImg.onload  = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src     = img[i].src;
+    }    
+  }
+  document.addEventListener('DOMContentLoaded', loadbar, false);
+}());
